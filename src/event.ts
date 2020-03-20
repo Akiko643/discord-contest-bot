4538ad1da1927664b199810f231d9439dfde0e83
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-20 20:39:31 +0800

    Add messages for notifyIntervals

--- src/event.ts
+++ src/event.ts
@@ -18,9 +18,9 @@ const day = 24 * hour;
 
 // notifyIntervals defines when bot should notify users about upcoming events
 export const notifyIntervals = [
-  1 * day,
-  2 * hour,
-  1 * hour,
-  30 * minute,
-  5 * minute,
+  { remainingTime: 1 * day, message: 'one day' },
+  { remainingTime: 2 * hour, message: 'two hours' },
+  { remainingTime: 1 * hour, message: 'one hour' },
+  { remainingTime: 30 * minute, message: '30 minutes' },
+  { remainingTime: 5 * minute, message: 'five minutes' },
 ];
